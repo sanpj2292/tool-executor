604cc3eae03f421f4b442d33b169adcaa06c95d9
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-28 14:49:04 +0530

    Changing the Icons for Website commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,12 @@
 import React from "react";
+import { ReactComponent as ToolSvg } from '../../icons/web-maintenance.svg';
 
 const Header = () => {
     return (
         <div className="navbar navbar-dark bg-dark">
             <a className="navbar-brand" href='/'>
-                <img src="../../icons/clipboard-play-multiple-outline.svg"
-                    width="30" height="30"
-                    className="d-inline-block align-top" alt="" />
-                    Tool Executor
+                <ToolSvg width={20} height={20} className="mr-1" />
+                <span className='align-center'>Tool Executor</span>
             </a>
         </div>
     );
